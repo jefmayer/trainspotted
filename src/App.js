63c23ac0ec58bc74c7bb73096125a6485a839f42
Jef Mayer <modeseventyeight@gmail.com>
2019-05-08 09:50:45 -0500

    First commit, added sass to create react app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './scss/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Trainspotted
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
