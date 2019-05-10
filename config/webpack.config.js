44fc78050820e2401844123ebf2f5a45beca4b27
Jef Mayer <modeseventyeight@gmail.com>
2019-05-10 12:45:42 -0500

    Committing latest updates to git/trying to run on heroku maybe...

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -26,6 +26,7 @@ const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
 
+const stylelint = require('stylelint');
 const postcssNormalize = require('postcss-normalize');
 
 // Source maps are resource heavy and can cause out of memory issue for large source files.
